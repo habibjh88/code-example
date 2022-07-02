8a9413c8e3ae5aa3d02541681e97cab2fe62e48a
Habib Rahaman <habibjh88@gmail.com>
2022-07-02 18:40:02 +0600

    ajax code

---
+++ Random/ajax.js
@@ -0,0 +1,27 @@
+<script>
+var data = {
+	action: 'textdomain_templeate_builder',
+	post_id: post_id ? post_id : null,
+	rttpg_nonce: textdomain_tb.rttpg_nonce
+};
+
+$.ajax({
+	url: textdomain_tb.ajaxurl,
+	data: data,
+	type: "POST",
+	beforeSend: function () {
+		modal.addModal().addLoading();
+	},
+	success: function (response) {
+		modal.removeLoading();
+		//console.log( response )
+		modal.addTitle(response.title);
+		if (response.success) {
+			modal.content(response.content);
+		}
+	},
+	error: function (e) {
+
+	}
+});
+</script>
