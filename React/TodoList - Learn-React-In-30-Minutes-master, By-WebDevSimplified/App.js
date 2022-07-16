f40b87cc17046e1c7258852501d0dc860cf6a16c
Habib Rahaman <habibjh88@gmail.com>
2022-07-16 17:17:12 +0600

    new react code added

---
+++ React/TodoList - Learn-React-In-30-Minutes-master, By-WebDevSimplified/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const LOCAL_STORAGE_KEY = 'local.storage';
+
+export default function App() {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        const getTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        setTodos(getTodos);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
+
+    const handleAddItem = () => {
+        const name = inputRef.current.value;
+        if (name === '') return
+        let id = Math.random() * 10;
+        setTodos(prev => {
+            return [...prev, { id, name, complete: false }]
+        })
+        console.dir(inputRef.current)
+        inputRef.current.value = '';
+        inputRef.current.focus()
+    }
+
+    function handleCheck(id) {
+        const newTodo = [...todos];
+        const item = newTodo.find((todo) => todo.id === id);
+        item.complete = !item.complete;
+        setTodos(newTodo)
+    }
+
+    function handleRemove() {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
+
+    return (
+        <div>
+            {todos.length > 0 && (
+                <div>Todo List:</div>
+            )}
+            {
+                todos.map(todo => {
+                    return (
+                        <div key={todo.id}>
+                            <label>
+                                <input onChange={() => handleCheck(todo.id)} type="checkbox" checked={todo.complete} />
+                                {todo?.name}
+                            </label>
+                        </div>
+                    )
+                })
+            }
+
+            <input ref={inputRef} type="text" />
+            <button onClick={handleAddItem}>Add Item</button>
+            <button onClick={handleRemove}>Remove Item</button>
+            <p>{todos.filter(todo => !todo.complete).length} item remain</p>
+        </div>
+    )
+}
