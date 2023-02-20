24dfe0ac564157698fbbadcca23e8b486ffc987a
Habib Rahaman <habibjh88@gmail.com>
2023-02-20 16:33:58 +0600

    some code added

--- WordPress/AAAAA/Post view count wordpress.php
+++ WordPress/AAAAA/Post view count wordpress.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Post view count
+ * Post view count (The post grid)
  * ====================================================================
  */
 add_filter( 'wp_head', 'update_post_views_count');
@@ -29,3 +29,80 @@ function update_post_views_count( $post_id ) {
 		}
 	}
 }
+
+// Remove transient after every 24 hours
+add_action('rttpg_daily_scheduled_events', function(){
+	try {
+		global $wpdb;
+		$expired = $wpdb->get_col( "SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE '_transient_timeout%' AND option_value < UNIX_TIMESTAMP()" );
+
+		foreach ( $expired as $transient ) {
+			$key = str_replace('_transient_timeout_tpg_cache_', 'tpg_cache_', $transient);
+			delete_transient( $key );
+		}
+
+	} catch ( \Exception $e ) {
+
+	}
+});
+
+/**
+ * Post View count By TT
+ * ============================================================================
+ */
+
+ //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Set post view on single page
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//---------------------------------------------------------------------
+// Set Post view Count
+//---------------------------------------------------------------------
+// 1
+
+
+// Set post count 
+// 2 
+if ( ! function_exists( 'set_post_view_count' ) ) :
+
+    function set_post_view_count( $contents ) {
+        if ( is_single() ) {
+			$postID = get_the_ID();
+			$count_key = 'tt_post_views_count';
+			$count = get_post_meta($postID, $count_key, true);
+			if($count==''){
+				$count = 0;
+				delete_post_meta($postID, $count_key);
+				add_post_meta($postID, $count_key, '0');
+			}else{
+				$count++;
+				update_post_meta($postID, $count_key, $count);
+			}
+        }
+
+        return $contents;
+    }
+
+    add_filter( 'the_content', 'set_post_view_count', 9999 );
+
+endif;
+
+
+// Get post view number 
+// 3
+if (!function_exists('get_post_views')) {
+	function get_post_views($postID){
+		$count_key = 'post_views_count';
+		$count = get_post_meta($postID, $count_key, true);
+		if($count==''){
+			delete_post_meta($postID, $count_key);
+			add_post_meta($postID, $count_key, '0');
+			return 0;
+		}
+		return $count;
+	}
+}
+
+
+
+
