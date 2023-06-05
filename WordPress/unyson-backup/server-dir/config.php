37d217c84f903364b53ea91b1c5f0b68ee7c08df
Habib Rahaman <habibjh88@gmail.com>
2023-06-05 11:17:23 +0600

    new dir created

---
+++ WordPress/unyson-backup/server-dir/config.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+
+ * Overwrite default config
+
+ */
+
+
+
+$cfg = array(
+
+	'files' => array(
+
+		'demo1' => dirname(__FILE__). '/demo.zip',
+
+		'demo2' => dirname(__FILE__). '/demo.zip',
+
+		'demo3' => dirname(__FILE__). '/demo.zip',
+
+		'demo4' => dirname(__FILE__). '/demo.zip',
+
+		'demo5' => dirname(__FILE__). '/demo.zip',
+
+		'demo6' => dirname(__FILE__). '/demo.zip',
+
+	),
+
+);
